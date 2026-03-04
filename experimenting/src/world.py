ee542f253809436a8fdcb2b1367e366fb999db40
PerroBetto <milo8600@gmail.com>
2026-03-03 19:10:42 -0700

    Reapply "Updated some docstrings, and made a small change to the .gitignore."
    This reverts commit fb4c52a7c4249ef65b06d96d68877f766b127bd4.

--- experimenting/src/world.py
+++ experimenting/src/world.py
@@ -1,4 +1,18 @@
+"""
+World class module.
 
+Manages all items in the game world (rooms, entities, items, etc.)
+
+
+* Creates the level layout.
+  > Includes saving and loading the game.
+
+* Calls the loop of each respective entity.
+* Handles requests its aggregates.
+
+Ideally I believe there should only be one world at a time, managing levels, a single player, etc.
+(makes sense, let me know)
+"""
 import typing_extensions
 import pygame
 from pygame import locals
